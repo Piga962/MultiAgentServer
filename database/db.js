4ae17b9d81a02b91b07d8094ea92fa376b10ab43
Piga962 <cesarjrmecinas@gmail.com>
2024-09-03 20:23:00 -0600

    error de conexion arreglado

--- database/db.js
+++ database/db.js
@@ -5,9 +5,12 @@ const Pool = require('pg').Pool;
 const db = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: 5432,
-    database: 'MultiAgents'
+    host: process.env.DB_HOST||'bicholover.up.railway.app',
+    port: 3000,
+    database: 'MultiAgents',
+    ssl:{
+        rejectUnauthorized: false
+    }
 })
 
 module.exports = {db};
