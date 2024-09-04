f9de2b7a0130e0cb3b68b82948b0c68a284498fb
Piga962 <cesarjrmecinas@gmail.com>
2024-09-03 20:28:06 -0600

    Fixed timout

--- database/db.js
+++ database/db.js
@@ -8,6 +8,9 @@ const db = new Pool({
     host: process.env.DB_HOST||'bicholover.up.railway.app',
     port: 3000,
     database: 'MultiAgents',
+    connectionTimeoutMillis: 10000,
+    idleTimeoutMillis: 30000,
+    max: 20,
 })
 
 module.exports = {db};
