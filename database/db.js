87dc96eadb595fd50b735482c4e90d9417bf0c2b
Piga962 <cesarjrmecinas@gmail.com>
2024-09-03 21:26:24 -0600

    Xd2

--- database/db.js
+++ database/db.js
@@ -3,14 +3,7 @@ dotenv.config();
 const Pool = require('pg').Pool;
 
 const db = new Pool({
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    host: process.env.PGHOST||'bicholover.up.railway.app',
-    port: process.env.PGPORT||3000,
-    database: process.env.PGDATABASE||'MultiAgent',
-    connectionTimeoutMillis: 10000,
-    idleTimeoutMillis: 30000,
-    max: 20,
+    connectionString: process.env.DATABASE_PUBLIC_URL
 })
 
 module.exports = {db};
