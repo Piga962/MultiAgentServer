2bae7e9f3b1f201e879784e0009754070d2709aa
Piga962 <cesarjrmecinas@gmail.com>
2024-09-03 21:18:20 -0600

    Ya

--- database/db.js
+++ database/db.js
@@ -3,17 +3,14 @@ dotenv.config();
 const Pool = require('pg').Pool;
 
 const db = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST||'bicholover.up.railway.app',
-    port: 3000,
-    database: 'MultiAgents',
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.PGHOST||'bicholover.up.railway.app',
+    port: process.env.PGPORT||3000,
+    database: process.env.PGDATABASE||'MultiAgent',
     connectionTimeoutMillis: 10000,
     idleTimeoutMillis: 30000,
     max: 20,
-    ssl: {
-        rejectUnauthorized: false
-    }
 })
 
 module.exports = {db};
